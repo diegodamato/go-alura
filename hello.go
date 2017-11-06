3a2579042c2ee0118dba415e29404f5ec88e887d
diego <diego.damato>
2017-11-06 16:32:08 -0200

    Finalizando aula7 e o curso

--- hello.go
+++ hello.go
@@ -4,9 +4,11 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"reflect"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -17,6 +19,7 @@ const delay = 5
 func main() {
 
 	exibeIntroducao()
+	registraLog("site-falso", false)
 
 	for {
 		exibeMenu()
@@ -28,6 +31,7 @@ func main() {
 			iniciarMonitoramento()
 		case 2:
 			fmt.Println("Exibindo logs...")
+			imprimeLogs()
 		case 0:
 			fmt.Println("Saindo do Programa...")
 			os.Exit(0)
@@ -90,8 +94,10 @@ func testaSite(site string) {
 
 	if resp.StatusCode == 200 {
 		fmt.Println("Site", site, "foi carregado com sucesso")
+		registraLog(site, true)
 	} else {
 		fmt.Println("Site", site, "esta com problemas. Status Code:", resp.StatusCode)
+		registraLog(site, false)
 	}
 }
 
@@ -115,10 +121,34 @@ func leSitesDoArquivo() []string {
 		if err == io.EOF {
 			break
 		}
-
 	}
 
 	arquivo.Close()
 
 	return sites
+}
+
+func registraLog(site string, status bool) {
+
+	arquivo, err := os.OpenFile("log.txt", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	arquivo.WriteString(time.Now().Format("02/01/2006 15:04:05") + " " + site + " - online: " + strconv.FormatBool(status) + "\n")
+
+	arquivo.Close()
+}
+
+func imprimeLogs() {
+
+	arquivo, err := ioutil.ReadFile("log.txt")
+
+	if err != nil {
+		fmt.Println("Ocorreu um erro:", err)
+	}
+
+	fmt.Println(string(arquivo))
+
 }
